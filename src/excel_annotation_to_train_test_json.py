5ed76c9d64f76a1beede7fdefcede802c6bf7588
Osman Mutlu <osmanmutlu92@gmail.com>
2023-03-01 19:57:39 +0300

    excel_annotation_to_train_test_json.py: Matching previously added ids in case of overlap

--- src/excel_annotation_to_train_test_json.py
+++ src/excel_annotation_to_train_test_json.py
@@ -17,7 +17,6 @@ def write_df_to_json(df, out_filename):
             out_d = {col: row[col] for col in cols}
             f.write(json.dumps(out_d, ensure_ascii=False) + "\n")
 
-
 to_be_processed = []
 if os.path.isdir(input_folder_or_filename):
     for filename in glob("{}/**/*.xlsx".format(input_folder_or_filename), recursive=True):
@@ -25,8 +24,8 @@ if os.path.isdir(input_folder_or_filename):
 elif input_folder_or_filename.endswith(".xslx"):
     to_be_processed = [input_folder_or_filename]
 
-
-all_df = pd.DataFrame()
+all_out_column_names = []
+all_df = pd.DataFrame(columns=["id", "text"])
 for filename in to_be_processed:
     df = pd.read_excel(filename)
     annotators = df.Annotator.unique().tolist()
@@ -34,23 +33,35 @@ for filename in to_be_processed:
     final_annot = annotators[0]
 
     task_names = [col_name for col_name in df.columns if re.search(r"^[IBT] ?\n", col_name)] #  col_name.startswith("I") or col_name.startswith("B")
-    out_column_names = []
+    curr_out_column_names = []
     for task_name in task_names:
         final_task_name = re.sub(r"^[ibt] ?\n", "", task_name.lower()).replace(" ", "_").replace("\n", "_")
         # final_task_name = task_name.lower().replace("i\n", "").replace("b\n", "").replace("i \n", "").replace(" ", "_")
-        out_column_names.append(final_task_name)
+        curr_out_column_names.append(final_task_name)
         # first set to one of the annotators
         df.loc[df.Annotator == "final", final_task_name] = df[df.Annotator == final_annot].reset_index(drop=True)[task_name].tolist()
-        # then set to final annotator if available
+        # then set to "final" annotator if available
         df.loc[(df.Annotator == "final") & (~df[task_name].isna()), final_task_name] = df[(df.Annotator == "final") & (~df[task_name].isna())].reset_index(drop=True)[task_name].tolist()
 
+    all_out_column_names.extend(curr_out_column_names)
+
     df = df[df.Annotator == "final"]
-    df = df[["#", "full_text"] + out_column_names]
+    df = df[["#", "full_text"] + curr_out_column_names]
     df = df.rename({"#": "id", "full_text": "text"}, axis=1)
     df.id = df.id.astype(int)
+
+    matched_ids = all_df[all_df.id.isin(df.id.tolist())].id.tolist()
+    if len(matched_ids) > 0:
+        matched_df = df[df.id.isin(matched_ids)]
+        for col_name in curr_out_column_names:
+            all_df.loc[all_df.id.isin(matched_ids), col_name] = matched_df[col_name].tolist()
+
+        df = df[~df.id.isin(matched_ids)]
+
     all_df = all_df.append(df, ignore_index=True)
 
-for col_name in out_column_names:
+all_out_column_names = list(set(all_out_column_names))
+for col_name in all_out_column_names:
     all_df[col_name] = all_df[col_name].astype(int)
 
     if len(all_df[all_df[col_name] == 1]) < 2:
@@ -82,5 +93,5 @@ for col_name in out_column_names:
     # test_df.to_json("{}/{}/test.json".format(out_folder, col_name), orient="records", lines=True, force_ascii=False)
     # dev_df.to_json("{}/{}/dev.json".format(out_folder, col_name), orient="records", lines=True, force_ascii=False)
 
-write_df_to_json(all_df, "{}/all.json".format(out_folder))
+write_df_to_json(all_df[["id", "text"] + all_out_column_names], "{}/all.json".format(out_folder))
 # all_df.to_json("{}/all.json".format(out_folder), orient="records", lines=True, force_ascii=False)
